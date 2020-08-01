8aa640b036f90bb40600557cde2c8465d8aee38d
papas000 <papas000@gmail.com>
2020-08-01 04:39:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import speech_recognition
 
+# Get audio from microphone
 recognizer = speech_recognition.Recognizer()
 with speech_recognition.Microphone() as source:
     print("Say something!")
     audio = recognizer.listen(source)
 
-print("Google Speech Recognition thinks you said: ")
-print(recognizer.recognize_google(audio))
+# Recognize speech using Google
+words = recognizer.recognize_google(audio)
+
+# Respond
+if "hello" in words:
+    print("Hello there!")
+elif "how are you" in words:
+    print("I'm fine, thanks!")
+elif "goodbye" in words:
+    print("Goodbye to you too!")
+else:
+    print("No capisco")
